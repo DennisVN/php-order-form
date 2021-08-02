3d35ab22c293ba7d6e51bb84faf7d84deea7602b
DennisVN <78357110+DennisVN@users.noreply.github.com>
2021-08-02 16:11:47 +0200

    Second error commit ssh difficulties

---
+++ index.php
@@ -0,0 +1,122 @@
+<?php
+
+// This file is your starting point (= since it's the index)
+// It will contain most of the logic, to prevent making a messy mix in the html
+
+// This line makes PHP behave in a more strict way
+declare(strict_types=1);
+
+ini_set('display_errors', '1');
+ini_set('display_startup_errors', '1');
+error_reporting(E_ALL);
+
+// We are going to use session variables so we need to enable sessions
+session_start();
+
+// Storing session data (sensitive stuff)
+$_SESSION["email"] = ($_POST['email']);
+$_SESSION["street"] = ($_POST['street']);
+$_SESSION["streetnumber"] = ($_POST['streetnumber']);
+$_SESSION["zipcode"] = ($_POST['zipcode']);
+
+// Setting a Cookie 
+
+// Storing Cookies
+$_COOKIE = [];
+if(isset($_POST['products'])){
+    setcookie("cookie", json_encode($product), time()+30*24*60*60);
+    echo "Cookie products set : " . ($_POST['products']);
+} else{
+    echo "Cookie storage failed";
+}
+
+// Use this function when you need to need an overview of these variables // FEEDBACK: TYPO
+function whatIsHappening() {
+    echo '<pre>';
+    echo '<h2>$_GET</h2>';
+    var_dump($_GET);
+    echo '<h2>$_POST</h2>';
+    var_dump($_POST);
+    echo '<h2>$_COOKIE</h2>';
+    var_dump($_COOKIE);
+    echo '<h2>$_SESSION</h2>';
+    var_dump($_SESSION);
+    echo '</pre>';
+}
+whatIsHappening();
+
+// TODO: provide some products (you may overwrite the example)
+$products = [
+    ['name' => 'Minecraft Platinum Trophy', 'price' => 999],
+    ['name' => 'DOOM (2016) Platinum Trophy', 'price' => 999],
+    ['name' => 'Dukem Nukem FOREVER Platinum Trophy', 'price' => 999],
+];
+
+$totalValue = 0;
+
+function validate()
+{
+    // This function will send a list of invalid fields back
+    $errors = [];
+
+    if (empty($_POST['email'])) {
+        $errors[] = 'email';
+    }
+    if (empty($_POST['street'])) {
+        $errors[] = 'street';
+    }
+    if (empty($_POST['streetnumber'])) {
+        $errors[] = 'streetnumber';
+    }
+    if (empty($_POST['zipcode'])) {
+        $errors[] = 'zipcode';
+    }
+    if (empty($_POST['products'])) {
+        $errors[] = 'products';
+    }
+
+    return $errors;
+}
+
+function handleForm($products)
+{   
+    $invalidFields = validate();
+    if (!empty($invalidFields)) {
+        $message = '';
+        foreach ($invalidFields as $invalidField) {
+            $message .= "please provide your {$invalidField}.";
+            $message .= '<br>';
+        }
+
+        return [
+            'errors' => true, 
+            'message' => $message 
+        ]; 
+
+    } else {
+        $productNumbers= array_keys($_POST['products']);
+        $productNames = [];
+        foreach ($productNumbers as $productNumber) {
+            $productNames[] = $products[$productNumber]['name'];
+        }
+        $message = 'You picked the following trophies : <br> ' . implode (',',$productNames);
+        $message .= '<br>';
+        $message .= 'Your email address : ' . $_POST['email'];
+        $message .= '<br>';
+        $message .= 'Your address : ' . $_POST['street'] . ' ' . $_POST['streetnumber'] . ', ' . $_POST['zipcode'] . ' ' . $_POST['city'];
+        return [
+            'errors' => false, 
+            'message' => $message
+        ];
+    }
+}
+
+// TODO: replace this if by an actual check
+$formSubmitted =  !empty($_POST);
+$result = [];
+
+if ($formSubmitted) {
+    $result = handleForm($products);
+}
+
+require 'form-view.php';
